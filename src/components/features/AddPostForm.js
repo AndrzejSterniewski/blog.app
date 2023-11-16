8522ad99a16452b00cac7ae11a5782a81b89e32c
Andrzej Sterniewski <drapiszon@gmail.com>
2023-11-16 14:51:22 +0100

    working on adding new post functionality

--- src/components/features/AddPostForm.js
+++ src/components/features/AddPostForm.js
@@ -1,28 +1,50 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addPost } from '../../redux/postsRedux';
+import { useNavigate } from 'react-router-dom';
+import shortid from "shortid";
 
 const AddPostForm = () => {
+
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [published, setPublished] = useState('');
+    const [description, setDescription] = useState('');
+    const [content, setContent] = useState('');
+
+    const navigate = useNavigate();
+
+    const dispatch = useDispatch();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch(addPost({ id: shortid(), title, author, published, description, content }));
+        navigate('/');
+    };
+
     return (
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 {/* <Form.Group className="mb-3" controlId="formBasicEmail"> */}
                     <Form.Label>Title</Form.Label>
-                    <Form.Control type="text" placeholder="Enter title" />
+                    <Form.Control type="text" placeholder="Enter title" value={title} onChange={e => setTitle(e.target.value)}/>
                 {/* </Form.Group> */}
                 {/* <Form.Group> */}
                     <Form.Label>Author</Form.Label>
-                    <Form.Control type="text" placeholder="Enter author" />
+                    <Form.Control type="text" placeholder="Enter author" value={author} onChange={e => setAuthor(e.target.value)}/>
                 {/* </Form.Group> */}
                 {/* <Form.Group> */}
                     <Form.Label>Published</Form.Label>
-                    <Form.Control type="text" placeholder="Enter date" />
+                    <Form.Control type="text" placeholder="Enter date" value={published} onChange={e => setPublished(e.target.value)}/>
                 {/* </Form.Group> */}
                 {/* <Form.Group> */}
                     <Form.Label>Short description</Form.Label>
-                    <Form.Control as="textarea" placeholder="Leave a comment" style={{ height: '100px' }} />
+                    <Form.Control as="textarea" placeholder="Leave a comment" style={{ height: '100px' }} value={description} onChange={e => setDescription(e.target.value)}/>
                 {/* </Form.Group> */}
                 {/* <Form.Group> */}
                     <Form.Label>Main content</Form.Label>
-                    <Form.Control as="textarea" placeholder="Main text" style={{ height: '100px' }} />
+                    <Form.Control as="textarea" placeholder="Main text" style={{ height: '100px' }} value={content} onChange={e => setContent(e.target.value)}/>
                 {/* </Form.Group> */}
                 <Button variant="primary" type="submit">
                     Submit
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -1,3 +1,5 @@
+import shortid from "shortid";
+
 // selectors
 export const getAllPosts = (state => state.posts);
 export const getPostById = ({ posts }, id) => posts.find(post => post.id === id);
@@ -5,15 +7,19 @@ export const getPostById = ({ posts }, id) => posts.find(post => post.id === id)
 // actions
 const createActionName = actionName => `app/posts/${actionName}`;
 const DELETE_POST = createActionName('DELETE_POST');
+const ADD_POST = createActionName('ADD_POST');
 
 // action creators
 export const deletePost = payload => ({ type: DELETE_POST, payload });
+export const addPost = payload => ({type: ADD_POST, payload});
 
 const postsReducer = (statePart = [], action) => {
     switch (action.type) {
         case DELETE_POST:
             return statePart.filter(post => post.id !== action.payload);
-        default:
+        case ADD_POST:
+            return [...statePart, {...action.payload, id: shortid() }];
+            default:
             return statePart;
     };
 };
